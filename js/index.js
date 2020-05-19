b52beef192234c0805b2a2255dec3acf165e961e
Roberto Sanchez <rsanchez@robpc.localdomain>
2020-05-18 18:02:48 -0600

    Cambios X

--- js/index.js
+++ js/index.js
@@ -3,3 +3,10 @@ import App from './app/App'
 
 const root = document.getElementById('app')
 render(<App></App>,root)
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+        .then( reg => {
+            console.log('Registrated')
+        })
+} 
