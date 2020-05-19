a89e5c6c9f2a100b6734d79f06c671035a98c76f
Roberto Sanchez <rsanchez@robpc.localdomain>
2020-05-18 18:46:01 -0600

    Added Add Todo

--- js/app/pages/TodoPage.js
+++ js/app/pages/TodoPage.js
@@ -1,12 +1,62 @@
 import  { h, Fragment } from 'preact'
+import {useState} from 'preact/hooks'
+import { Input, Form, Button, Modal, DatePicker } from 'antd'
+
 import TodoItems from '../components/test'
 
+
+
 const TodoPage = (props) => {
+    const [todoItems, setTodoItems] = useState([])
+    const [openModal, setOpenModal] = useState(false)
+    const [form] = Form.useForm()
+    const addTodo = e => {
+        const todoRawValues = form.getFieldsValue()
+       const todo = {
+           todo: todoRawValues.todo,
+           date: todoRawValues.date.format('YYYY-MM-DD')
+       }
+       console.log(todo)
+       setTodoItems([...todoItems, todo])
+        form.resetFields()
+        toggleForm()
+    }
+    
+    const toggleForm = e => {
+        setOpenModal(!openModal)
+    }
+
+ 
 
     return (
     <Fragment>
         <h1> Todo Page</h1>
-        <TodoItems></TodoItems>
+        <Button onClick={toggleForm}> Add Todo</Button>
+        <Modal title='Add Form'
+            visible={openModal}
+            onCancel={toggleForm}
+            footer={[
+                <Button onClick={toggleForm} >
+                    Cancel
+                </Button>,
+                <Button onClick={addTodo}>Submit</Button>
+            ]}
+            >
+                <Form form={form} onFinish={addTodo}>
+                <Form.Item
+                label='Todo' 
+                name='todo'>
+                    <Input/>
+                    </Form.Item> 
+                    <Form.Item
+                        label='Date'
+                        name='date'
+                    >
+                        <DatePicker ></DatePicker>
+                    </Form.Item>
+                </Form>
+        </Modal>
+        <TodoItems todos={todoItems}></TodoItems>
     </Fragment>
 
 
--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+//import 'preact/debug'
+import 'preact/devtools'
+
 import {h , render } from 'preact'
 import App from './app/App'
+import 'antd/dist/antd.css'
+
 
 const root = document.getElementById('app')
 render(<App></App>,root)
