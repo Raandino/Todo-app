0f31ef21521ca0e2d566eeabff48675853713ccf
Roberto Sanchez <rsanchez@robpc.localdomain>
2020-05-18 18:46:57 -0600

    Documentacion

--- js/app/pages/TodoPage.js
+++ js/app/pages/TodoPage.js
@@ -11,14 +11,19 @@ const TodoPage = (props) => {
     const [openModal, setOpenModal] = useState(false)
     const [form] = Form.useForm()
     const addTodo = e => {
+        // Gets Raw Data from the Form
         const todoRawValues = form.getFieldsValue()
+        // Transform the Date into usable format
        const todo = {
            todo: todoRawValues.todo,
            date: todoRawValues.date.format('YYYY-MM-DD')
        }
        console.log(todo)
+       // Adds the Todo Item to the array
        setTodoItems([...todoItems, todo])
+       // Resets Form
         form.resetFields()
+        // Closes the Form
         toggleForm()
     }
     
