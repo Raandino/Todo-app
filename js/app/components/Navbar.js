6e649b03a9d27b5ff67d46601d497aee1d0a5240
AlexC01 <alexcuadraxxx@gmail.com>
2020-05-18 19:22:38 -0600

    no se

--- js/app/components/Navbar.js
+++ js/app/components/Navbar.js
@@ -4,16 +4,9 @@ import { Button } from 'antd/lib/button/button';
 
 
 const Navbar = (props) => {
-<<<<<<< HEAD
-    return (
-    <nav>
-        <Button type='primary'>
-            <Link activeClassName="active" href="/"> Todo page</Link>
-=======
     return (<nav>
         <Button type='pri'>
             <Link activeClassName="active" href="/">! Todo page !</Link>
->>>>>>> 7e2db008e5374bf984c51e053622f1eb58462878
         </Button>
         <Button>
             <Link activeClassName="active" href="/login">! Login page !</Link>
